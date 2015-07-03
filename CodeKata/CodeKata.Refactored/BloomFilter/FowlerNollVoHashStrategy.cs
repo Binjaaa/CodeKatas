289038808493f2ac69102fcc33b0727d45cfc5cb
Binjaaa <binya87@gmail.com>
2015-07-03 23:51:47 +0200

    Add hash function implementations

---
+++ CodeKata/CodeKata.Refactored/BloomFilter/FowlerNollVoHashStrategy.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeKata.Refactored.BloomFilter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Fowler–Noll–Vo hash function - https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+    /// </summary>
+    public class FowlerNollVoHashStrategy : NonCriptographyHashStrategy
+    {
+        public override UInt64 GetHash(string str)
+        {
+            return this.GetFNVHash(str);
+        }
+
+        private UInt64 GetFNVHash(string s)
+        {
+            //FNV_offset_basis
+            UInt64 fnvOffsetBasis = 14695981039346656037;
+
+            //FNV prime
+            UInt64 fnvPrime = 1099511628211;
+
+            byte[] valueToHash = Encoding.UTF8.GetBytes(s);
+            foreach (byte actualByte in valueToHash)
+            {
+                unchecked
+                {
+                    fnvOffsetBasis ^= actualByte;
+                    fnvOffsetBasis *= fnvPrime;
+                }
+            }
+
+            return fnvOffsetBasis;
+        }
+    }
+}
---
+++ CodeKata/CodeKata.Refactored/BloomFilter/NonCriptographyHashStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CodeKata.Refactored.BloomFilter
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public abstract class NonCriptographyHashStrategy
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public abstract UInt64 GetHash(string str);
+    }
+}
